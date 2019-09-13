206cb07c0a0135c1bde41df3199fc285628912de
Aris <ariskak14@gmail.com>
2019-09-13 04:13:28 +0300

    commit

--- variables.php
+++ variables.php
@@ -2,6 +2,13 @@
 <h3> php</h3>
 
 <?php 
-$numberRandom = 'test';
-echo $numberRandom
+// $name = array('bread' => 3, 'moss' => 4);
+// $name2 = [3 => 'bread', 'moss' =>4];
+// echo $name['bread'];
+// echo $name2[3];
+
+$nameArrays = array('a' => 1, 'b'=>2, 'c'=>3, 'd'=>4, 'e'=>5);
+foreach($nameArrays as $nameArray => $nameNumber){
+    echo ' '.$nameArray. ': ' .$nameNumber. ' ,';
+}
 ?>
